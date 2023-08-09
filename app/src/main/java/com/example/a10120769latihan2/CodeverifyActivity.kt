eb74902ac6f004a6fb3c38f454ef0d50d1c475eb
Rifa nur anisa <rifanura12@gmail.com>
2023-08-09 14:52:00 +0700

    Latihan 2

---
+++ app/src/main/java/com/example/a10120769latihan2/CodeverifyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a10120769latihan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CodeverifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_almostthere)
+    }
+}
---
+++ app/src/main/java/com/example/a10120769latihan2/registerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a10120769latihan2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class registerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
